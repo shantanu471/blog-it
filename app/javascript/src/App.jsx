db7ceb95015bcc8dc80ca35743dab4a528120391
shantanu471 <a.shantanu@pixelcompute.com>
2025-03-24 06:55:08 +0000

    Fixed Ui

--- app/javascript/src/App.jsx
+++ app/javascript/src/App.jsx
@@ -3,15 +3,13 @@ import React from "react";
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
 import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
 
- import BlogPosts from "components/BlogPosts";
- import Lists from "components/Lists";
+import BlogPosts from "components/BlogPosts";
 
 const App = () => {
   return (
     <Router>
       <Switch>
         <Route exact path="/blogs" component={BlogPosts} />
-        <Route exact path="/lists" component={Lists} />
         <Redirect path="*" to="/blogs" />
       </Switch>
     </Router>
--- app/javascript/src/components/Sidebar/index.jsx
+++ app/javascript/src/components/Sidebar/index.jsx
@@ -5,20 +5,17 @@ import { NavLink } from "react-router-dom";
 
 const Sidebar = () => (
   <div className="fixed flex h-screen w-16 flex-col items-center border-r border-gray-200 bg-white py-4">
+
+    <Book className="h-12 w-12 bg-black text-white my-2 rounded-lg p-2" />
+
     <NavLink
-      activeClassName="bg-black text-white"
+      activeClassName="text-black"
       className="my-2 rounded-lg p-2 hover:bg-gray-400"
       to="/blogs"
     >
-      <Book className="h-6 w-6" />
-    </NavLink>
-    <NavLink
-      activeClassName="bg-black text-white"
-      className="my-2 rounded-lg p-2 hover:bg-gray-400"
-      to="/lists"
-    >
       <List className="h-6 w-6" />
     </NavLink>
+
     <div className="mt-4 flex h-full flex-col justify-end">
       <div className="h-12 w-12 rounded-full bg-gray-300" />
     </div>
