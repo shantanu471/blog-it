eb2cbbf30f3e03dfa17f48e336e12cb4a3edb62a
shantanu471 <a.shantanu@pixelcompute.com>
2025-03-26 07:35:14 +0000

    Added slug to task

--- app/models/post.rb
+++ app/models/post.rb
@@ -18,4 +18,29 @@ class Post < ApplicationRecord
     numericality: { only_integer: true, greater_than_or_equal_to: 0 }
   validates_inclusion_of :is_bloggable,
     in: [true, false]
+  validates :slug,
+    uniqueness: true
+
+  validate :slug_not_changed
+
+  before_create :set_slug
+
+  private
+
+    def set_slug
+      itr = 1
+      loop do
+        title_slug = title.parameterize
+        slug_candidate = itr > 1 ? "#{title_slug}-#{itr}" : title_slug
+        break self.slug = slug_candidate unless Post.exists?(slug: slug_candidate)
+
+        itr += 1
+      end
+    end
+
+    def slug_not_changed
+      if will_save_change_to_slug? && self.persisted?
+        errors.add(:slug, I18n.t("task.slug.immutable"))
+      end
+    end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2025_03_13_095926) do
+ActiveRecord::Schema[7.1].define(version: 2025_03_26_072207) do
   create_table "posts", force: :cascade do |t|
     t.string "title", null: false
     t.text "description", null: false
@@ -19,6 +19,8 @@ ActiveRecord::Schema[7.1].define(version: 2025_03_13_095926) do
     t.boolean "is_bloggable", default: false, null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "slug", null: false
+    t.index ["slug"], name: "index_posts_on_slug", unique: true
   end
 
 end
