5180cc0e4b86f1e9abfde16d765bbf49d1474745
shantanu471 <a.shantanu@pixelcompute.com>
2025-04-06 14:04:18 +0000

    Restructured components

---
+++ app/javascript/src/components/Lists/index.jsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import { Typography } from "@bigbinary/neetoui";
+
+const Lists = () => (
+  <div className="ml-24 w-auto px-4 py-8">
+    <Typography className="mb-8 text-5xl font-bold text-gray-800">
+      Lists
+    </Typography>
+  </div>
+);
+
+export default Lists;
---
+++ app/javascript/src/components/Main.jsx
@@ -0,0 +1,69 @@
+import React, { useState } from "react";
+
+import classnames from "classnames";
+import { Route, Switch } from "react-router-dom";
+import { Redirect } from "react-router-dom/cjs/react-router-dom.min";
+import { ToastContainer } from "react-toastify";
+
+import CreatePost from "components/CreatePost";
+import Lists from "components/Lists";
+import BlogPosts from "components/Posts";
+import ShowPost from "components/ShowPost";
+import Sidebar from "components/Sidebar";
+
+const App = () => {
+  const [categorySearched, setCategorySearched] = useState("");
+  const [selectedCategories, setSelectedCategories] = useState([]);
+  const [isSidebarOpen, setIsSidebarOpen] = useState(false);
+
+  const handleCategorySearch = searchTerm => {
+    setCategorySearched(searchTerm);
+  };
+
+  const handleCategorySelect = categories => {
+    setSelectedCategories(categories);
+  };
+
+  const handleSidepane = isSidepaneOpen => {
+    setIsSidebarOpen(isSidepaneOpen);
+  };
+
+  return (
+    <div className="flex h-screen w-full overflow-hidden">
+      <ToastContainer />
+      <Sidebar
+        onCategorySearch={handleCategorySearch}
+        onCategorySelect={handleCategorySelect}
+        onSidebarOpen={handleSidepane}
+      />
+      <div
+        className={classnames(
+          "flex-1 overflow-auto transition-all duration-300 ease-in-out",
+          {
+            "ml-80": isSidebarOpen,
+            "ml-0": !isSidebarOpen,
+          }
+        )}
+      >
+        <Switch>
+          <Route exact component={ShowPost} path="/posts/:slug/show" />
+          <Route exact component={CreatePost} path="/posts/create" />
+          <Route
+            exact
+            path="/blogs"
+            render={() => (
+              <BlogPosts
+                categorySearched={categorySearched}
+                selectedCategories={selectedCategories}
+              />
+            )}
+          />
+          <Route exact component={Lists} path="/lists" />
+          <Redirect path="*" to="/blogs" />
+        </Switch>
+      </div>
+    </div>
+  );
+};
+
+export default App;
