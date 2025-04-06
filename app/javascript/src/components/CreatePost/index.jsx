07a73fe3af19c656fb48beac36a02439a22b6826
shantanu471 <a.shantanu@pixelcompute.com>
2025-04-06 13:48:18 +0000

    Added create post functionality

--- app/javascript/src/components/CreatePost/index.jsx
+++ app/javascript/src/components/CreatePost/index.jsx
@@ -1,21 +1,38 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 import { Typography } from "@bigbinary/neetoui";
 import { useHistory } from "react-router-dom/cjs/react-router-dom.min";
 
+import categoriesApi from "apis/categories";
 import postsApi from "apis/posts";
+import usersApi from "apis/users";
 
 import PostForm from "./Form";
 
 const CreatePost = () => {
   const [loading, setLoading] = useState(false);
+  const [categories, setCategories] = useState();
 
   const history = useHistory();
 
   const handleSubmit = async values => {
     setLoading(true);
     try {
-      await postsApi.create(values);
+      const response = await usersApi.fetch();
+      const defaultUser = response.data.users[0];
+
+      const category_ids = values.new_post_categories.map(
+        category => category.value
+      );
+
+      const postData = {
+        ...values,
+        assigned_user_id: defaultUser.id,
+        assigned_organization_id: defaultUser.assigned_organization_id,
+        category_ids,
+      };
+
+      await postsApi.create(postData);
       history.push("/blogs");
     } catch (error) {
       logger.error(error);
@@ -24,6 +41,29 @@ const CreatePost = () => {
     }
   };
 
+  const fetchCategories = async () => {
+    try {
+      const response = await categoriesApi.fetch();
+
+      const categories_posts = response.data.categories;
+
+      setCategories(
+        categories_posts.map(category => ({
+          label: category.category_name,
+          value: category.id,
+        }))
+      );
+      setLoading(false);
+    } catch (error) {
+      logger.error(error);
+      setLoading(false);
+    }
+  };
+
+  useEffect(() => {
+    fetchCategories();
+  }, []);
+
   return (
     <div className="mx-24 w-auto gap-y-8 px-4 py-8">
       <Typography className="text-5xl font-bold text-gray-800">
@@ -31,10 +71,15 @@ const CreatePost = () => {
       </Typography>
       <div className="flex-grow-1 mt-10 border-2 px-10 py-6">
         <PostForm
+          categories={categories}
           handleSubmit={handleSubmit}
-          initialValues={{ title: "", description: "" }}
           loading={loading}
           type="create"
+          initialValues={{
+            title: "",
+            description: "",
+            new_post_categories: [],
+          }}
           onCancel={() => history.push("/blogs")}
         />
       </div>
